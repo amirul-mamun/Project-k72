838fc11ade817b7a060ac86d0fefc9a3cf0f8dc9
amirul-mamun <amriulislam21354@gmail.com>
2026-05-29 08:16:58 +0600

    Navber completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import { Link, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Agence from './pages/Agence'
 import Projects from './pages/Projects'
+import Navbar from './components/other/Navbar'
+import FullScreenNav from './components/other/FullScreenNav'
 
 const App = () => {
   return (
     <div className=' text-white' >
-
+        {/* <Navbar/>
       <Routes>
          <Route path='/' element ={<Home/>} />
          <Route path='/agence' element ={<Agence/>} />
          <Route path='/projects' element ={<Projects/>} />
-      </Routes>
+      </Routes> */}
+      <FullScreenNav/>
     </div>
   )
 }
---
+++ src/components/other/FullScreenNav.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const FullScreenNav = () => {
+  return (
+    <div className='bg-black py-50 h-screen w-full absolute' >
+       <div className='flex items-center justify-center w-full'></div>
+      <div>
+        <div className='link border-t-2 relative overflow-hidden'>
+             <h1  className=' text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]'>Projects</h1>
+              <div className='flex absolute bg-lime-300 top-0 moveLink text-black ' >
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+              </div>
+        </div>
+        <div className='link border-y-2 relative overflow-hidden'>
+             <h1  className=' text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]'>Agence</h1>
+              <div className='flex absolute bg-lime-300 top-0 moveLink text-black ' >
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+              </div>
+        </div>
+        <div className='link border-y-2 relative overflow-hidden'>
+             <h1  className=' text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]'>Contact</h1>
+              <div className='flex absolute bg-lime-300 top-0 moveLink text-black ' >
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+              </div>
+        </div>
+        <div className='link border-y-2 relative overflow-hidden'>
+             <h1  className=' text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]'>Blogue</h1>
+              <div className='flex absolute bg-lime-300 top-0 moveLink text-black ' >
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+                <div className='flex items-center move ' >
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>                 
+                    <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1779079516892-d1b8bc4dd64a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+                    <h2 className='whitespace-nowrap text-[8.3vw] uppercase text-center leading-[0.85] pb-[0.5vw]' >Pour Tout voir</h2>
+                     <img className=' h-32 w-[14vw] shrink-0 rounded-full object-cover' src="https://images.unsplash.com/photo-1778074896004-0ed12c0bbf32?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </div>
+              </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default FullScreenNav
